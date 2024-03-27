e55228ba95e252d91ae59cc9725a674d9f31a3f3
MuhiaKevin <archerkreiger@gmail.com>
2024-03-27 21:42:39 +0300

    Finished parsing list but need to explain in comments what is happening

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    if let Some(n) =  encoded_value
-        .strip_prefix('i') // cut out the 'i' and get the rest of the text
-        .and_then(|rest| rest.split_once('e'))  // split the string by and get the number from the
-        // string
-        .and_then(|(digits, _)| digits.parse::<i64>().ok())  // convert the string number to an
-    // actual interger
-    {
-        return n.into(); // convert the i64 to serde_json::Value
-    } 
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
 
-    if let Some((len, rest)) = encoded_value.split_once(':') {
-        if let Ok(len) = len.parse::<usize>() {
-            return  serde_json::Value::String(rest[..len].to_string())
+    match encoded_value.chars().next() {
+        Some('i') => {
+            if let Some((n, rest)) =  encoded_value
+                .split_at(1).1
+                .split_once('e')
+                .and_then(|(digits, rest)| {
+                    let n = digits.parse::<i64>().ok()?;
+                    Some((n, rest))
+                })
+            {
+                return  (n.into(), rest);
+            }
         }
+
+        Some('l') => {
+            let mut values = Vec::new();
+            let mut rest = encoded_value.split_at(1).1;
+
+            while !rest.is_empty() && !rest.starts_with('e') {
+                let (v, remainder) = decode_bencoded_value(rest);
+                values.push(v);
+                rest = remainder;
+            }
+            
+            return (values.into(), &rest[1..])
+        }
+
+        Some('0'..='9') => {
+            if let Some((len, rest)) = encoded_value.split_once(':') { // get the length of string
+                // and the string separately
+                if let Ok(len) = len.parse::<usize>() { // convert the length of the string to u16
+                    return (rest[..len].to_string().into(), &rest[len..])
+                } 
+            }
+        }
+
+        _ => {}
     }
 
     panic!("Unhandled encoded value: {}", encoded_value)
@@ -28,7 +52,7 @@ fn main() {
         let encoded_value = &args[2];
 
         let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value);
+        println!("{}", decoded_value.0);
 
     } else {
         println!("unkown command: {:?}", command);

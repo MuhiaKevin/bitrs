96f0b8e9b8efc37d312bbdae340bf354ef4542ba
MuhiaKevin <archerkreiger@gmail.com>
2024-03-28 13:34:19 +0300

    Slow steps moving towards using serde_bencode

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,42 @@ mod parse_bencode;
 
 // from file parse_bencode (parse_bencode.rs) import function decode_bencoded_value
 use parse_bencode::decode_bencoded_value;
+use serde::Deserialize;
+
+#[derive(Debug, Clone, Deserialize)]
+struct Torrent {
+    announce: String,
+    info: Info,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct Info {
+    name: String,
+
+    #[serde(rename = "piece length")]
+    plength: usize,
+    pieces: Vec<u8>,
+
+    #[serde(flatten)]
+    keys: Keys,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+enum Keys{
+    SingleFile{
+        length: usize,
+    },
+    MultiFile{
+        files: Vec<File>,
+    },
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct File {
+    length: usize,
+    path: Vec<String>,
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -10,11 +46,17 @@ fn main() {
 
     if command == "decode" {
         let encoded_value = &args[2];
-
-        let decoded_value = decode_bencoded_value(encoded_value);
+        //
+        // let mut buffer = String::new();
+        // std::io::stdin().read_line(&mut buffer).unwrap();
+        //
+        //
+        // let decoded_value = decode_bencoded_value(&buffer);
+        let decoded_value = decode_bencoded_value(&encoded_value);
         println!("{}", decoded_value.0);
 
     } else {
         println!("unkown command: {:?}", command);
     }
 }
+

e40c4e88a03091ec0ae7a82a1925cd4cf3658856
MuhiaKevin <archerkreiger@gmail.com>
2024-03-28 08:35:20 +0300

    Can now parse dictionary

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,36 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
                     Some((n, rest))
                 })
             {
+                // TODO: read more on the .into() bcoz i dont understand it
+                // https://dev.to/richardbray/type-conversion-in-rust-as-from-and-into-493l
+                // https://www.reddit.com/r/rust/comments/p69c9w/what_does_the_into_method_do/
+                // https://doc.rust-lang.org/rust-by-example/conversion/from_into.html
                 return  (n.into(), rest);
             }
+        }
+
+        Some('d') => {
+            let mut dict = serde_json::Map::new(); // create an empty vec/list
+            let mut rest = encoded_value.split_at(1).1; // get rest of list from the letter 'l',
+
+            // first check that rest &str is not empty and it doesnt start with e
+            while !rest.is_empty() && !rest.starts_with('e') {
+                let (k, remainder) = decode_bencoded_value(rest);
+
+                // use shadowing to replace k with something else 
+                // check to see if the k is of type enum variant string and return the string 
+                // if not panic the whole program and exit
+                let k = match k { 
+                    serde_json::Value::String(k) => k,
+                    _ => panic!("Dict keys must be of type string")
+                };
+
+                let (v, remainder) = decode_bencoded_value(remainder);
+                dict.insert(k, v); // saves decoded values of the list into a vector
+                rest = remainder;
+            }
+            
+            return (serde_json::Value::Object(dict.into()), &rest[1..])
         }
 
         Some('l') => {
@@ -22,9 +50,7 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
 
             // first check that rest &str is not empty and it doesnt start with e
             while !rest.is_empty() && !rest.starts_with('e') {
-                // println!("REST of List: {rest}");
                 let (v, remainder) = decode_bencoded_value(rest);
-                println!("v: {v} remainder: {remainder}");
                 values.push(v); // saves decoded values of the list into a vector
                 rest = remainder;
             }
@@ -46,7 +72,6 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
 
     panic!("Unhandled encoded value: {}", encoded_value)
 }
-
 
 
 fn main() {
